7f19f7f649df5438b746618fe510608386e70bf0
85Evren <92681265+85Evren@users.noreply.github.com>
2021-11-06 03:07:57 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Odev_30102021_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long sayi;//--
+
+            Console.WriteLine("1. Sayıyı Giriniz");
+            sayi = long.Parse(Console.ReadLine());
+
+            if (sayi == 0)
+            {
+                Console.WriteLine("Girilen Sayı Sıfıra Eşittir.");
+                Console.ReadKey();
+            }
+            else if (sayi > 0)
+            {
+                Console.WriteLine("Girilen Sayı Pozitiftir.");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Girilen Sayı Negatiftir.");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
