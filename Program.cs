b6c5f4c50ed318ceb864b4f8bed92e83d55471f1
85Evren <92681265+85Evren@users.noreply.github.com>
2021-11-06 03:08:30 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,29 +1,73 @@
 ﻿using System;
 
-namespace Odev_30102021_1
+namespace SınavSonucu
 {
+
     class Program
     {
+
         static void Main(string[] args)
         {
-            long sayi;//--
+            int vize1;
+            int vize2;
+            int final;
+            double sonuc;
 
-            Console.WriteLine("1. Sayıyı Giriniz");
-            sayi = long.Parse(Console.ReadLine());
+            Console.WriteLine("1. Vize Notunu Giriniz.");
+            vize1 = int.Parse(Console.ReadLine());
 
-            if (sayi == 0)
+            Console.WriteLine("2. Vize Notunu Giriniz.");
+            vize2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Final Notunu Giriniz.");
+            final = int.Parse(Console.ReadLine());
+
+            sonuc = ((vize1 + vize2) / 2) * 0.30 + (final * 0.70);
+
+
+            if (sonuc >= 0 && sonuc < 39)
             {
-                Console.WriteLine("Girilen Sayı Sıfıra Eşittir.");
+                Console.WriteLine("Sınav Sonucu FF'dir. Sınav Notu:" + sonuc);
                 Console.ReadKey();
             }
-            else if (sayi > 0)
+            else if (sonuc >= 39 && sonuc < 46)
             {
-                Console.WriteLine("Girilen Sayı Pozitiftir.");
+                Console.WriteLine("Sınav Sonucu FD'dir. Sınav Notu:" + sonuc);
+                Console.ReadKey();
+            }
+            else if (sonuc >= 46 && sonuc < 53)
+            {
+                Console.WriteLine("Sınav Sonucu DD'dir. Sınav Notu:" + sonuc);
+                Console.ReadKey();
+            }
+            else if (sonuc >= 53 && sonuc < 60)
+            {
+                Console.WriteLine("Sınav Sonucu DC'dir. Sınav Notu:" + sonuc);
+                Console.ReadKey();
+            }
+            else if (sonuc >= 60 && sonuc < 67)
+            {
+                Console.WriteLine("Sınav Sonucu CC'dir. Sınav Notu:" + sonuc);
+                Console.ReadKey();
+            }
+            else if (sonuc >= 67 && sonuc < 74)
+            {
+                Console.WriteLine("Sınav Sonucu CB'dir. Sınav Notu:" + sonuc);
+                Console.ReadKey();
+            }
+            else if (sonuc >= 74 && sonuc < 81)
+            {
+                Console.WriteLine("Sınav Sonucu BB'dir. Sınav Notu:" + sonuc);
+                Console.ReadKey();
+            }
+            else if (sonuc >= 81 && sonuc < 87)
+            {
+                Console.WriteLine("Sınav Sonucu BA'dir. Sınav Notu:" + sonuc);
                 Console.ReadKey();
             }
             else
             {
-                Console.WriteLine("Girilen Sayı Negatiftir.");
+                Console.WriteLine("Sınav Sonucu AA'dir. Sınav Notu:" + sonuc);
                 Console.ReadKey();
             }
 
